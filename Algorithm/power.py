3347928297aafd6ac63049a859a19b692df03eef
Jihwan Lee <wlghks7790@gmail.com>
2024-05-16 21:52:19 +0900

    240516 study decrease-and-conquer with a power problem

--- Algorithm/power.py
+++ Algorithm/power.py
@@ -15,16 +15,17 @@ def power(x, n):
     else:
         return x * power(x*x, (n-1)/2)
 
-import time
-print("억지기법(2*500) =", bf_power(2.0, 500))
-print("축소정복(2*500) =", power(2.0, 500))
+if  __name__ == "__main__":
+    import time
+    print("억지기법(2*500) =", bf_power(2.0, 500))
+    print("축소정복(2*500) =", power(2.0, 500))
 
-t1 = time.time()
-for i in range(10**5) : bf_power(2.0, 500) # 억지 기법
-t2 = time.time()
-for i in range(10**5) : power(2.0, 500) # 축소 정복
-t3 = time.time()
+    t1 = time.time()
+    for i in range(10**5) : bf_power(2.0, 500) # 억지 기법
+    t2 = time.time()
+    for i in range(10**5) : power(2.0, 500) # 축소 정복
+    t3 = time.time()
 
-print("억지기법 시간...", t2-t1)
-print("축소정복 시간...", t3-t2)
+    print("억지기법 시간...", t2-t1)
+    print("축소정복 시간...", t3-t2)
 
