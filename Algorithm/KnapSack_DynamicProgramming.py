94ace58e2b65bf522fa74d355b3005f1cdeff8d5
Jihwan Lee <wlghks7790@gmail.com>
2024-06-08 21:11:38 +0900

    240608 study Dynamic Programming with Knapsack problem

--- Algorithm/KnapSack_DynamicProgramming.py
+++ Algorithm/KnapSack_DynamicProgramming.py
@@ -13,11 +13,22 @@ def knapSack_bf(W, wt, val, n):
 
 # 동적 계획법
 def knapSack_dp(W, wt, val, n):
-    pass
-    
+    A = [[0 for _ in range(W+1)] for _ in range(n+1)] # 테이블 초기화
+
+    for i in range(1, n+1): # 진행방향: 위에서 아래
+        for w in range(1, W+1): # 진행방향: 좌에서 우
+            if wt[i-1] > w: # i번째 물건이 용량 초과
+                A[i][w] = A[i-1][w]
+            else: # i번째 물건을 넣을 수 있음
+                valWith = val[i-1] + A[i-1][w-wt[i-1]] # i번째 물건을 넣는 경우
+                valWithout = A[i-1][w] # i번째 물건을 넣지 않는 경우
+                A[i][w] = max(valWith, valWithout)
+    return A[n][W]
+
 if __name__ == "__main__":
     val = [60, 100, 190, 120, 200, 150] 
     wt = [2, 5, 8, 4, 7, 6] 
     W = 18
     n = len(val) 
-    print("0-1배낭문제(분할 정복): ", knapSack_bf(W, wt, val, n)) 
+    print("0-1배낭문제(분할 정복): ", knapSack_bf(W, wt, val, n))
+    print(knapSack_dp(W, wt, val, n))
--- Algorithm/LongestCommonSubsequence.py
+++ Algorithm/LongestCommonSubsequence.py
@@ -7,13 +7,38 @@ def lcs_recur(X, Y, m, n): # 문자열 X, Y와 그 길이 m, n
     else:
         return max(lcs_recur(X, Y, m-1, n), lcs_recur(X, Y, m, n-1))
     
+# LCS 길이 계산 알고리즘 with Dynamic Programming
 def lcs_dp(X, Y):
     m = len(X)
     n = len(Y)
     L = [[None]*(n+1) for _ in range(m+1)] # 테이블 준비
 
     for i in range(m+1):
-        pass
+        for j in range(n+1):
+            if i == 0 or j == 0:
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]: # 마지막 글자가 같으면
+                L[i][j] = L[i-1][j-1] + 1
+            else: # 마지막 글자가 다르면
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+    
+    return L[m][n], L, lcs_dp_traceback(X, Y, L)
+
+def lcs_dp_traceback(X, Y, L): # L은 이미 계산되어 있는 상태이어야 함
+    lcs = ""
+    i = len(X) # 맨 마지막 인덱스로 초기화
+    j = len(Y) # 맨 마지막 인덱스로 초기화
+    while i > 0 and j > 0:
+        v = L[i][j]
+        if v > L[i][j-1] and v > L[i-1][j-1] and v > L[i-1][j]:
+            i -= 1
+            j -= 1
+            lcs = X[i] + lcs
+        elif v == L[i][j-1] and v > L[i-1][j]:
+            j -= 1
+        else:
+            i -= 1
+    return lcs
 
 if __name__ == "__main__":
     x = 'HELLO WORLD'
@@ -21,4 +46,7 @@ if __name__ == "__main__":
     m = len(x)
     n = len(y)
 
-    print(lcs_recur(x, y, m, n)) # 재귀 활용
+    print(lcs_recur(x, y, m, n)) # lcs 길이 계산 with 재귀
+    print(lcs_dp(x, y)[0]) # lcs 길이 계산 with 동적 프로그래밍
+    print(lcs_dp(x, y)[1]) # lcs 테이블
+    print(lcs_dp(x, y)[2]) # lcs
